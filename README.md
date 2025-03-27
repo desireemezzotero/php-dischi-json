2b5cf5c47620a0526e417e5e92dcdb2f02bc2d78
desireemezzotero <desireemezzotero1@gmail.com>
2025-03-27 11:52:13 +0100

    add index end readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# PHP DISCHI JSON
+
+## Consegna
+
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+I dischi dovranno avere questa struttura: titolo, artista, url della cover, anno di pubblicazione, genere
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php dischi</title>
+
+  <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body class="bg-black">
+  <header>
+    <nav class="h-[80px]">
+    <h1 class="text-6xl font-extrabold text-white text-center pt-3">PHP DISCHI</h1>
+    </nav> 
+  </header>
+
+  <main>
+
+
+  </main>
+
+
+  
+</body>
+
+</html>
